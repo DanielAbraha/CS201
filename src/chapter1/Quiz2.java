ecaa92c5b9d20428d52e4bb70056f1ec3059277f
DanielAbraha <dannyabraha27@gmail.com>
2021-09-30 12:18:13 -0500

    30 sept

---
+++ src/chapter1/Quiz2.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+import java.util.Random;
+
+public class Quiz2 {
+    public static void main(String[] args) {
+        Random grade = new Random();
+        char grad = (char) (grade.nextInt(4) + ('A'));
+        for (int i = 0; i < 10; i++) {
+            System.out.println(grad);
+        }
+    }
+}
