ab23706e7a6248c92d68cf45aec705153e15d547
DanielAbraha <dannyabraha27@gmail.com>
2021-10-06 21:42:04 -0500

    06 oct

---
+++ src/lab6/BooleanExpressions.java
@@ -0,0 +1,51 @@
+package lab6;
+
+public class BooleanExpressions {
+    public static void main(String[] args) {
+            // Indent of if Statements.
+//         1) a. if(a==b)
+//                  if( c==d)
+//                      a=1;
+//                  else
+//                      b=1;
+//               else
+//                   c=1;
+//           b). if(a==b)
+//                  a=1;
+//                  if( c==d)
+//                    b=1;
+//                  else
+//                      c=1;
+//           c). if( a==b) {
+//            if (c == d)
+//                a = 1;
+//            b = 2;
+//        }    else
+//                b=1;
+//
+//
+//           d). if(a==b) {
+//                    if (c == d)
+//                         a = 1;
+//                       b = 2;
+//               }else {
+//                  b = 1;
+//                     if (a == d)
+//                     d = 3;
+//
+//            // The equivalent statements.
+//        2). a and b are equivalent.
+//
+//                    // Boolean Expressions
+//         3).     a.  false
+//        }        b. false. It is always false regardless the values of x,y.
+//                 c. true
+//                 d. true
+//                 e. false. It is always false regardless the values of x,y.
+//                 f. true
+//                 g. false
+//                 h. true
+//
+        }
+    }
+
